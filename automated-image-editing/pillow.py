a0ec9ca9aec6dbef930b97d42aa7f9a29bd0e789
Gabriel Trevisan <gabriel.trevisan.santos@gmail.com>
2025-08-23 11:50:49 -0300

    Lesson 02

--- automated-image-editing/pillow.py
+++ automated-image-editing/pillow.py
@@ -1,5 +1,6 @@
 from PIL import Image
 
+#Lesson 01
 image = Image.open("images/image.jpg")
 
 print(image.format)
@@ -13,3 +14,15 @@ print(image.info)
 image.resize(size=(200, 800)).save("images/image2.jpg")
 
 image.crop((0, 0, 300, 300)).save("images/image3.jpg")
+
+#Lesson 02
+image = Image.open("images/maca.jpg")
+r, g, b = image.split()
+
+Image.merge("RGB", (b, g, r)).save("images/maca2.jpg")
+
+image = Image.open("images/maca.jpg")
+image2 = Image.open("images/image.jpg")
+
+image.paste(image2)
+image.save("images/merge.jpg")
