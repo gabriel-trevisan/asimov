dc59e8bdafffc79a30d5b36597ec9ea7b3d6e790
Gabriel Trevisan <gabriel.trevisan.santos@gmail.com>
2025-08-27 08:01:06 -0300

    New Course

---
+++ exploring-ia-hugging-face/teste_ia.py
@@ -0,0 +1,14 @@
+from transformers import pipeline
+
+modelo = pipeline('fill-mask')
+
+frase = "The capital of <mask> is Brasilia."
+
+predicoes = modelo(frase)
+
+for predicao in predicoes:
+    resposta = predicao['token_str']
+    score = predicao['score']
+    frase = predicao['sequence']
+    score_ajustado = score * 100
+    print(f'Predição "{resposta}" com score {score_ajustado:.2f}% -> "{frase}"')
